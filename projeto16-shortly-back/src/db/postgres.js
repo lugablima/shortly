90c3dd4c5827656aa597c75d00382f1cadd29590
LucasLima <lugablima@gmail.com>
2022-08-02 17:04:18 -0300

    chore: Prepare for deploy

--- projeto16-shortly-back/src/db/postgres.js
+++ projeto16-shortly-back/src/db/postgres.js
@@ -3,12 +3,23 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-const connection = new Pool({
+const databaseConfigProduction = {
+  connectionString: process.env.DATABASE_URL,
+  ss1: {
+    rejectUnauthorized: false,
+  },
+};
+
+const databaseConfigDev = {
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   database: process.env.DATABASE_NAME,
-});
+};
+
+const databaseConfig = process.env.NODE_ENV === "production" ? databaseConfigProduction : databaseConfigDev;
+
+const connection = new Pool(databaseConfig);
 
 export default connection;
