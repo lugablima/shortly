494a23f5f9a59dc285059bd1c0864bfff344b594
LucasLima <lugablima@gmail.com>
2022-08-04 14:48:11 -0300

    chore: add GET route '/urls/:id'

--- projeto16-shortly-back/src/controllers/urlsController.js
+++ projeto16-shortly-back/src/controllers/urlsController.js
@@ -22,5 +22,22 @@ export async function createShortUrl(req, res) {
 }
 
 export async function getShortUrl(req, res) {
-  //
+  const { id } = req.params;
+
+  try {
+    const {
+      rows: [shortUrl],
+    } = await connection.query(
+      `SELECT id, "shortUrl", url FROM "shortUrls"
+    WHERE id = $1`,
+      [id]
+    );
+
+    if (!shortUrl) return res.sendStatus(404);
+
+    res.status(200).send(shortUrl);
+  } catch (err) {
+    console.log("Error getting short url", err.message);
+    res.sendStatus(500);
+  }
 }
